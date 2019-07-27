6cc045831ae09a5adff28105f4393c95dff4d255
Syarol <1oleq25@gmail.com>
2019-07-27 20:04:25 +0300

    Added simple cookies message

--- src/js/index.js
+++ src/js/index.js
@@ -43,6 +43,26 @@ modeSwitchers.map(el => {
 });
 
 /**
+ * Accepting using cookies
+**/
+
+var isCookiesAccepted = localStorage.getItem('cookies-allowed') ? localStorage.getItem('cookies-allowed') : null; //gets saved state from localStorage
+
+var cookiesMessage = document.getElementsByClassName('cookies-message')[0];
+
+/*if using of cookies is allowed then remove the message, else - adds a listener for cookies accepting*/
+if (isCookiesAccepted) {
+	cookiesMessage.remove();
+} else {
+	let acceptCookiesBtn = document.getElementsByClassName('cookies-accept-btn')[0];
+	acceptCookiesBtn.onclick = () => {
+		cookiesMessage.remove();
+		localStorage.setItem('cookies-allowed', true);
+	}
+}
+
+
+/**
  * Hiding/Showing of header panel, inpage navigation
 **/
 
